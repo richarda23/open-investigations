5618ee5c3889e4690cd2b8c7d93863fa153c766c
richarda23 <richard.c.adams@infinityworks.com>
2023-11-20 08:48:37 +0000

    alternative text

--- audio_analyser.py
+++ audio_analyser.py
@@ -18,40 +18,39 @@ def transcribe(audio_file):
     return transcript
 
 
-def complexity(transcription):
+def do_message(system, user=None):
+    messages = [
+        {
+            "role": "system",
+            "content": f"{system}"
+        }
+    ]
+    if user is not None:
+        messages.append({
+            "role": "system",
+            "content": f"{user}"
+        })
+
     response = client.chat.completions.create(
         model="gpt-4",
         temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": f"Please explain what reading level and typical age is needed to understand the text"
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
+        messages=messages
     )
     return response.choices[0].message.content
+
+
+def complexity(transcription):
+    return do_message("Please explain what reading level and typical age is needed to understand the text",
+                      transcription)
 
 
 def goodexpl(subject, input2):
-    response = client.chat.completions.create(
-        model="gpt-4",
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": f"Please explain if this is a good explanation for {subject}"
-            },
-            {
-                "role": "user",
-                "content": input2
-            }
-        ]
-    )
-    return response.choices[0].message.content
+    return do_message(f"Please explain if this is a good explanation for {subject}. Don't suggest an alternative "
+                      f"explanation", input2)
+
+
+def alternative(subject):
+    return do_message(f"Please provide a brief explanation of {subject} suitable for a 7 year old ")
 
 
 # Main function for Streamlit app
@@ -64,6 +63,7 @@ def main():
             - Record a short  audio file (e.g. 30 seconds) of you describing a complicated topic
             - Upload it here (must be m4a, mp3, mp4, mpeg format)
             - App will transcribe to text and comment on the reading-age needed to understand the text
+            - App will also comment on whether the explanation is a good one 
             - Powered by OpenAI's Whisper
         """
         )
@@ -72,7 +72,7 @@ def main():
     if uploaded_file is not None:
         st.session_state.file_bytes = uploaded_file
 
-    title = st.text_input('What are you trying to talk about?')
+    subject = st.text_input('What are you trying to talk about?')
     if st.button("Analyse"):
         with st.spinner("Analysing..."):
             text = transcribe(st.session_state.file_bytes)
@@ -82,14 +82,17 @@ def main():
             analysis = complexity(text)
             st.write(analysis)
             st.header("Relevance")
-            rel = goodexpl(title, text)
+            rel = goodexpl(subject, text)
             st.write(rel)
+            st.header("Alternative suitable for 7 year old")
+            alt = alternative(subject)
+            st.write(alt)
 
     # Create a button with label 'Clear Input'
     if st.button("Clear Input"):
         # Clear the text area
-        st.experimental_rerun()
         st.session_state.file_bytes = None
+        st.experimental_rerun()
 
 
 if __name__ == "__main__":
